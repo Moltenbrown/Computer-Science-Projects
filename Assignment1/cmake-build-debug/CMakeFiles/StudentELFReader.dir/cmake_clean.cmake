f8cb337251d0e75c6ae477810a734e61068f107b
Moltenbrown <ucm@case.edu>
2019-01-04 00:27:09 -0800

    More of the documents I created.

---
+++ Assignment1/cmake-build-debug/CMakeFiles/StudentELFReader.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StudentELFReader.dir/elf_common.c.o"
+  "libStudentELFReader.pdb"
+  "libStudentELFReader.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/StudentELFReader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
