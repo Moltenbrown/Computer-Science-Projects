d79870251b800485aadf9ca8aaafc3c905376ff7
ugochi <madubata.u@husky.neu.edu>
2019-10-27 18:25:12 -0700

    attempting to fix my code for the rundatatransformer but realizing the issue may have been with the depth code in the first place.

--- CS6200/Assignment2/RunDataTransformer.py
+++ CS6200/Assignment2/RunDataTransformer.py
@@ -5,13 +5,23 @@ import sys
 file_directory = sys.argv[1]
 amount_to_parse = sys.argv[2]
 
+output_file = open("output.txt", "wb")
+
 files = os.listdir(file_directory)
 for file in files:
 	print(file)
 	file_name = file_directory + "/" + file
 	with open(file_name, "rb") as current_file:
+		output_file.write(bytes(file_name, encoding='utf8'))
+		output_file.write(bytes('\n', encoding='utf8'))
 		file_data = current_file.read()
-		tokens = re.findall(rb"(([a-z]|[A-Z])*(?!\.|\w+\:)[^>\s\"]+)", file_data)
-		print(tokens)
+		tokens = re.findall(rb"(?<!^<)([a-z]|[A-Z])+(?![0-9])", file_data)
+		for token in tokens:
+			output_file.write(bytes(''.join(str(letter) for letter in token), encoding='utf8'))
+			output_file.write(bytes('\n', encoding='utf8'))
 	current_file.closed
+	output_file.write(bytes('\n', encoding='utf8'))
+
+output_file.close()
+
 
