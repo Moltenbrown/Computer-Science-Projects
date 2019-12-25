4e5d21e29a4dd4a7e591153416689c307cd7f583
madubata <madubata.u@husky.neu.edu>
2019-12-24 17:00:39 -0800

    Progress on documentID. Just have to write the structure to a json file and it should be done.

--- CS6200/Assignment2/CreateIndex.py
+++ CS6200/Assignment2/CreateIndex.py
@@ -1,3 +1,4 @@
+from DocumentID import documentID
 from TermID import termID
 import sys
 
@@ -5,3 +6,6 @@ fileToIndex = sys.argv[1]
 
 termID = termID()
 termID.generateTermID(fileToIndex)
+
+documentID = documentID()
+documentID.generateDocumentID(fileToIndex)
