1396ae48e754965662dbab256e378f0d55a13102
madubata <madubata.u@husky.neu.edu>
2019-12-27 13:58:12 -0800

    Trying to get to create a final form of the program

--- CS6200/Assignment2/RunDataTransformer.py
+++ CS6200/Assignment2/RunDataTransformer.py
@@ -4,9 +4,9 @@ from Stats import stats
 
 counter = 0
 
-file_directory = sys.argv[1]
-amount_to_parse = sys.argv[2]
+def RunDataTransformer(file_directory, amount_to_parse):
+	stats_output = stats()
+	outputfile = stats_output.getStats(file_directory, amount_to_parse)
 
-stats_output = stats()
-stats_output.getStats(file_directory, amount_to_parse)
+	return outputfile
 
